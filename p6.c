f79ea790ecb2ec61da8681638195d5f154210772
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2023-06-02 06:54:44 +0530

    Practical 6 PPS OF GTU

---
+++ p6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+     
+    double c ;   
+    printf("Enter Temperature in Centigrade :- ");
+    scanf("%lf",&c);
+    printf("The Temperature in Fahrenheit is :- %lf",1.8*c +32);
+    return 0 ;
+}
+ 
+
